169f2eec2ed41b80c281ea53369122f317be5a6b
leonardosm14 <leosouzaam@hotmail.com>
2025-08-30 00:32:44 -0300

    limpeza de dados e conversão xlsx para csv.

---
+++ data/xlsx_to_csv.py
@@ -0,0 +1,24 @@
+#Script para converter dados em xlsx para csv e formatar nomes das colunas.
+import pandas as pd
+import os
+from unicodedata import normalize
+from unidecode import unidecode
+from pathlib import Path
+
+xlsx_folder = Path("raw_data/")
+csv_folder = Path("csv_data/")
+if not csv_folder.exists():
+    os.mkdir(csv_folder)
+
+for xlsx_file in xlsx_folder.iterdir():
+    print(f"Converting xlsx file {xlsx_file.name} to csv and formatting.")
+    df = pd.read_excel(io=xlsx_file)
+
+    #Remove acentuações, substitui ' - ' por um único espaço, substitui espaços por '_'.
+    df.columns = [
+        unidecode(
+            normalize("NFKD", col.replace(' - ', ' ').replace(' ', '_').lower())
+        ) for col in df.columns
+    ]
+
+    df.to_csv(path_or_buf=f"{csv_folder}/{xlsx_file.name}", index=False)
